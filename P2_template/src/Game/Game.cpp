7e33deed73db4f318c566e30e46b32e924c99804
RubiaLuque <prubia01@ucm.es>
2023-05-03 15:11:30 +0200

    plantilla arreglada. Version anterior comentada

--- P2_template/src/Game/Game.cpp
+++ P2_template/src/Game/Game.cpp
@@ -1,7 +1,7 @@
 #include "Game.h"
 #include "Player.h"
 
-Game::Game(){
+Game::Game() {
     // TODO create settings
     ROAD_WIDTH = 2000;
     ROAD_LENGTH = 10000;
@@ -10,74 +10,172 @@ Game::Game(){
     bDebug = false;
 }
 
-void Game::init(){
-    
-    if(gameObjects != nullptr)
+Game::~Game() {
+    ofLogNotice() << "Deleting game";
+    delete gameObjects;
+    delete generator;
+    delete currentState();
+}
+
+void Game::init() {
+
+    if (gameObjects != nullptr)
         delete gameObjects;
-    
+
+
     gameObjects = new GameObjectContainer();
-    
+
     player = new Player(this);
     player->init();
 
     cam.setPosition(0, 300, -600);
     cam.setTarget(player->transform);
     cam.setParent(player->transform);
-    
+    cam.disableMouseInput();
+    cam.setFarClip(100000);
+
     gameObjects->add(player);
     generator->generateWorld();
     bPlayerFinish = false;
     initTime = ofGetElapsedTimef();
 }
 
-void Game::update(){
+void Game::update() {
     gameObjects->update();
 }
 
-void Game::draw(){
+void Game::draw() {
     ofEnableLighting();
     ofEnableDepthTest();
-    
+
     cam.begin();
     {
-        if(bDebug) gameObjects->drawDebug();
+        if (bDebug) gameObjects->drawDebug();
         else gameObjects->draw();
     }
     cam.end();
-    
+
     ofDisableLighting();
     ofDisableDepthTest();
 }
 
 
-Player * Game::getPlayer(){
+Player* Game::getPlayer() {
     return player;
 }
 
-vector<GameObject *> Game::getCollisions(GameObject *gameObject){
+vector<GameObject*> Game::getCollisions(GameObject* gameObject) {
     return gameObjects->getCollisions(gameObject);
 }
 
-void  Game::addGameObject(GameObject *gameobject){
+void  Game::addGameObject(GameObject* gameobject) {
     gameObjects->add(gameobject);
 }
 
-void Game::finishGame(){
+void Game::finishGame() {
     bPlayerFinish = true;
 }
 
-void Game::toggleDebug(){
+void Game::toggleDebug() {
     bDebug = !bDebug;
 }
 
-bool Game::isFinished(){
+bool Game::isFinished() {
     return bPlayerFinish;
 }
 
-void Game::setFinished(bool v){
+void Game::setFinished(bool v) {
     bPlayerFinish = v;
 }
 
-float Game::getEllapsedTime(){
+float Game::getEllapsedTime() {
     return ofGetElapsedTimef() - initTime;
 }
+
+
+
+
+//VERSIÓN ANTIGUA CON ERRORES
+//#include "Game.h"
+//#include "Player.h"
+//
+//Game::Game(){
+//    // TODO create settings
+//    ROAD_WIDTH = 2000;
+//    ROAD_LENGTH = 10000;
+//
+//    generator = new GameObjectGenerator(this);
+//    bDebug = false;
+//}
+//
+//void Game::init(){
+//    
+//    if(gameObjects != nullptr)
+//        delete gameObjects;
+//    
+//    gameObjects = new GameObjectContainer();
+//    
+//    player = new Player(this);
+//    player->init();
+//
+//    cam.setPosition(0, 300, -600);
+//    cam.setTarget(player->transform);
+//    cam.setParent(player->transform);
+//    
+//    gameObjects->add(player);
+//    generator->generateWorld();
+//    bPlayerFinish = false;
+//    initTime = ofGetElapsedTimef();
+//}
+//
+//void Game::update(){
+//    gameObjects->update();
+//}
+//
+//void Game::draw(){
+//    ofEnableLighting();
+//    ofEnableDepthTest();
+//    
+//    cam.begin();
+//    {
+//        if(bDebug) gameObjects->drawDebug();
+//        else gameObjects->draw();
+//    }
+//    cam.end();
+//    
+//    ofDisableLighting();
+//    ofDisableDepthTest();
+//}
+//
+//
+//Player * Game::getPlayer(){
+//    return player;
+//}
+//
+//vector<GameObject *> Game::getCollisions(GameObject *gameObject){
+//    return gameObjects->getCollisions(gameObject);
+//}
+//
+//void  Game::addGameObject(GameObject *gameobject){
+//    gameObjects->add(gameobject);
+//}
+//
+//void Game::finishGame(){
+//    bPlayerFinish = true;
+//}
+//
+//void Game::toggleDebug(){
+//    bDebug = !bDebug;
+//}
+//
+//bool Game::isFinished(){
+//    return bPlayerFinish;
+//}
+//
+//void Game::setFinished(bool v){
+//    bPlayerFinish = v;
+//}
+//
+//float Game::getEllapsedTime(){
+//    return ofGetElapsedTimef() - initTime;
+//}
